2671acb9746cb44d751ef8dfd8e9f973b46c8033
ruslan_nasirov <Nasirov.r.m@mail.ru>
2025-12-12 20:51:05 -0800

    NewHomeWork

--- src/Main.java
+++ src/Main.java
@@ -4,76 +4,122 @@ public class Main {
     public static void main(String[] args) {
 
         // Задание 1
-
-        for (int i =1; i<= 10; i= i+1) {
-            System.out.println(i);
+        int firstFriday = 3;
+        for (int day = firstFriday; firstFriday <= 31; firstFriday = firstFriday + 7) {
+            System.out.println("Сегодня пятница, " + firstFriday + " число. Необходимо подготовить отчет");
         }
 
         // Задание 2
 
-        for (int i = 10; i > 0; i=i-1) {
-            System.out.println(i);
+        // Задание 2.1
+
+        int distance = 42_195;
+        int reminder = 500;
+
+        do {
+            System.out.println("Держись, осталось " + distance);
+            distance = distance - reminder;
+
+        } while (distance >= 0);
+
+
+        // Задание  2.2
+
+        for (int passed = 0; passed <= 42195; passed = passed + 500) {
+            // использовал цифры вместо переменной, потому, что с переменной идея не запускала второй вариант.
+            int finish = 42195 - passed;
+            System.out.println("Держить, осталось, " + finish + " метров");
         }
 
         // Задание 3
 
-        for (int i = 0; i <= 17; i=i+2){
-            System.out.println("Четные числа " + i);
+        // Задание 3.1
+
+        int money = 1000;
+        int day = 0;
+
+        while (money >= 100) {
+
+            day++;
+            money = money - 100;
+            if (day % 5 == 0) {
+                money = money + 100;
+                System.out.println("Сегодня " + day + " день, парковка БЕСПЛАТНО ");
+                continue;
+            }
+            System.out.println("Сегодня " + day + " день парковки, остаток средств " + money);
+
+
         }
+
+        // Задание 3.2
+
+        int balance = 1000;
+        int days = 0;
+
+        for (; balance >= 0; balance = balance - 100) {
+            days++;
+
+            if (days % 5 == 0) {
+                System.out.println("Сегодня " + days + " день, парковка БЕСПЛАТНАЯ");
+                balance = balance + 100;
+                continue;
+            }
+
+            System.out.println(days + " день парковки. Остаток средств " + balance);
+        }
+
 
         // Задание 4
 
-        for (int i = 10; i >= -10; i= i-1 ){
-            System.out.println(i);
-        }
+        int month = 0;
+        double total = 0;
+        while (total < 12_000_000) {
+            month++;
+            total = total + 15_000;
+
+            if (month % 6 == 0) {
+                total = total * 1.07;
+            }
+            {
+                // Строчку вывода в консоль, скопировал из " Обсуждения домашки"  честно признаю..
+                // не понимаю... Подскажи, пожалуйста.
+                System.out.printf("Месяц %d, сумма накоплений %.2f рублей%n ", month, total);
+
+
+                }
+            }
 
         // Задание 5
 
-        for (int i = 1904; i <= 2096; i = i+4){
-            System.out.println(i + " год является високосным");
-        }
+        // Привет Наставник!
+        // Подскажи, пожалуйста, что не так?
 
-        // Задание 6
+        int charge = 20;
+        int minute = 0;
+        int overheats = 0;
 
-        for (int i = 7; i <=98; i=i+7){
-            System.out.println(i);
-        }
+        while (charge < 100 && overheats <= 3) {
+            minute++;
+            charge = charge + 2;
+            if (minute % 10 == 0) {
+                overheats++;
+                minute += 2;
+                System.out.println(" Случился перегрев "+ overheats + ". Зарядка приостановленна. Заряд " + charge + "%" );
+                if (overheats == 3) {
+                    break;
+                }
+                continue;
+            }
 
-        // Задание 7
 
-        for (int i = 1; i<=512; i=i*2){
-            System.out.println(i);
-        }
+            System.out.println(" Текущий заряд "+ charge +" % ");
 
-        // Задание 8
 
-        int salary = 29000;
-        int total = 0;
 
-        for (int i = 1; i <= 12; i=i+1) {
-            total = total + salary;
-
-            System.out.println("Месяц " + i + " сумма накоплений составит " + total);
-        }
-
-        // Задание 9
-
-        int salary1 = 29000;
-        int savings = 0;
-
-        for (int i = 1; i<=12; i=i+1){
-            savings = savings + savings/100;
-            savings = savings + salary1;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + savings);
 
         }
 
-        // Задание 10
-
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("2*" + i + "=" + i * 2);
-        }
 
 
 
