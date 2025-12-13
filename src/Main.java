3e9fa3b9ed71b99ba5b86833ce115ef4458eb389
ruslan_nasirov <Nasirov.r.m@mail.ru>
2025-12-13 14:28:32 -0800

    NewHomeWork

--- src/Main.java
+++ src/Main.java
@@ -25,9 +25,11 @@ public class Main {
 
         // Задание  2.2
 
-        for (int passed = 0; passed <= 42195; passed = passed + 500) {
+        int distance1 = 42195;
+
+        for ( int passed = 0; passed <=distance1; passed = passed + 500) {
             // использовал цифры вместо переменной, потому, что с переменной идея не запускала второй вариант.
-            int finish = 42195 - passed;
+            int finish = distance1 - passed;
             System.out.println("Держить, осталось, " + finish + " метров");
         }
 
@@ -54,10 +56,12 @@ public class Main {
 
         // Задание 3.2
 
-        int balance = 1000;
+        // В этом задании на 1-й день выводит отстаток средств = 1000
+
+
         int days = 0;
 
-        for (; balance >= 0; balance = balance - 100) {
+        for (int balance = 1000; balance >= 0; balance = balance - 100) {
             days++;
 
             if (days % 5 == 0) {
@@ -82,43 +86,37 @@ public class Main {
                 total = total * 1.07;
             }
             {
-                // Строчку вывода в консоль, скопировал из " Обсуждения домашки"  честно признаю..
-                // не понимаю... Подскажи, пожалуйста.
-                System.out.printf("Месяц %d, сумма накоплений %.2f рублей%n ", month, total);
-
+                System.out.printf(" Месяц %d, сумма накоплений %.2f рублей %n ", month, total);
 
                 }
             }
 
         // Задание 5
 
-        // Привет Наставник!
-        // Подскажи, пожалуйста, что не так?
 
-        int charge = 20;
+        int charge = 30;
         int minute = 0;
         int overheats = 0;
 
         while (charge < 100 && overheats <= 3) {
             minute++;
             charge = charge + 2;
+
             if (minute % 10 == 0) {
                 overheats++;
                 minute += 2;
-                System.out.println(" Случился перегрев "+ overheats + ". Зарядка приостановленна. Заряд " + charge + "%" );
-                if (overheats == 3) {
+                System.out.println(" Случился перегрев "+ overheats + ". Зарядка приостановленна на 2 минуты. Заряд " + charge + "%" );
+                if (overheats >= 3) {
+                    System.out.println(" Случился 3-й перегрев. Зарядка аварийно приостановленна. Уровень заряда " + charge);
                     break;
                 }
                 continue;
             }
 
-
-            System.out.println(" Текущий заряд "+ charge +" % ");
-
-
-
+            System.out.println("Прошло времени " + minute + " min. Текущий заряд "+ charge +" % ");
 
         }
+
 
 
 
